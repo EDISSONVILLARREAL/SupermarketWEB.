31f2bcec380ff0b58e46f077618f7c738550a024
EDISSON VILLARREAL <cuentacecep@gmail.com>
2025-05-14 02:22:05 -0500

    mensaje de error al ingresar credenciales

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            {
+                return RedirectToPage("/index");
+            }
+
+            return Page();
+        }
+    }
+}
